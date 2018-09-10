a2dfd7fc2d1e066297605a1ec018e4a887433e35
Mary Tran <mtran063@uottawa.ca>
2018-09-10 16:10:16 -0400

    Created repository from existing project

---
+++ DNode.java
@@ -0,0 +1,37 @@
+/**
+ * A simple node class for a doubly-linked list.  Each node has a
+ * reference to a stored element, a previous node, and a next node.
+ * This class is based on the <code>DNode</code> class by Roberto Tamassia.
+ *
+ * @author Jochen Lang
+ * @author Mary Tran
+ */
+
+public class DNode {
+    private Object element;
+    private DNode prev;
+    private DNode next;
+  
+    DNode() { 
+        this(null, null, null); 
+    }
+
+    DNode(Object e, DNode prev, DNode next) {
+        element = e;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public void setElement(Object newElem) { element = newElem; }
+
+    public void setNext(DNode newNext) { next = newNext; }
+
+    public void setPrev(DNode newPrev) { prev = newPrev; }
+
+
+    public Object getElement() { return element; }
+
+    public DNode getNext() { return next; }
+
+    public DNode getPrev() { return prev; }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+Name: Vo Thanh Ngan (Mary) Tran
+Student number: 8696752
+Lab section: CSI 2120 A02
+Lab number: 1
+
+This directory contains DNode.java which is based on Node.java, as well as DLinkList.java, adapted to work with doubly linked nodes, and GLinkList.java, adapted to work with singly linked nodes using generics, based on LinkList.java. There's also a bunch of other things I didn't touch.
